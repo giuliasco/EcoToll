6df2631cbd4d39376ea59079954d7b99d8de27fd
Eribio <erika.biondi@student.univaq.it>
2019-08-01 16:22:19 +0200

    change classes Autostrada e Casello

--- EcoToll/src/application/model/Autostrada.java
+++ EcoToll/src/application/model/Autostrada.java
@@ -1,129 +1,60 @@
 package application.model;
-import java.util.List;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Autostrada {
 	
-	private StringProperty id=new SimpleStringProperty();
-	private StringProperty nome_autostrada=new SimpleStringProperty();
-	private StringProperty inizio=new SimpleStringProperty();
-	private StringProperty fine=new SimpleStringProperty();
-	private IntegerProperty km=new SimpleIntegerProperty();
-	private DoubleProperty tariffa_km= new SimpleDoubleProperty();
+	private int id;
+	private double km,tariffa_km;
+	private String nome_autostrada,inizio,fine;
 	
+	//costruttore vuoto
 	
+	public Autostrada() {}
 	
-	private final ObservableList<Autostrada> autostrade;
-	
-	public Autostrada() {
-		id=new SimpleStringProperty();
-		nome_autostrada=new SimpleStringProperty();
-		inizio=new SimpleStringProperty();
-		fine=new SimpleStringProperty();
-		km=new SimpleIntegerProperty();
-		tariffa_km= new SimpleDoubleProperty();
-		
-	
-		
-		autostrade=FXCollections.observableArrayList();
+	//costruttore che contiene tutti i campi
+	public Autostrada(int id, double km,double tariffa_km,String nome_autostrada, String inizio,String fine) {
+		this.id= id;
+		this.km= km;
+		this.tariffa_km= tariffa_km;
+		this.nome_autostrada= nome_autostrada;
+		this.inizio= inizio;
+		this.fine= fine;
 	}
 	
-/*metodi di get*/
-	public final String getId() {return id.get();}
-	public final String getNomeAutostrada() {return nome_autostrada.get();}
-	public final String getInizio() {return inizio.get();}
-	public final String getFine() {return fine.get();}
-	public final int getKm() {return km.get();}
-	public final double getTariffaKm() {return tariffa_km.get();}
+	//costruttore che contiene ResultSet (query DB)
 	
-	/*medodi di set*/
-	public final void setId(String value) {id.set(value);}
-	public final void setNomeAutostrada(String value) {nome_autostrada.set(value);}
-	public final void setInizio(String value) {inizio.set(value);}
-	public final void setFine(String value) {fine.set(value);}
-	public final void setKm(int value) {km.set(value);}
-	public final void setTariffaKm(double value) {tariffa_km.set(value);}
-	
-	//metodi di getter per le property
-	public StringProperty idProperty() {return id;}
-	public StringProperty nomeAutostradaProperty() {return nome_autostrada;}
-	public StringProperty inizioProperty() {return inizio;}
-	public StringProperty fineProperty() {return fine;}
-	public IntegerProperty kmProperty() {return km;}
-	public DoubleProperty tariffaKmProperty() {return tariffa_km;}
-	
-	public List<Autostrada> getElencoAutostrade(){ return this.autostrade;}
-	
-	public void setElencoAutostrade(List<Autostrada> autostrade) {
-		this.autostrade.setAll(autostrade);}
-	
-	public ObservableList<Autostrada> autostradaObsList(){return this.autostrade;}
-	
-	
-	
-	@Override
-	public String toString() {
-		return getId() ;
+	public Autostrada(ResultSet resultSet) throws SQLException{
+		this.setId(resultSet.getInt("id"));
+		this.setKm(resultSet.getDouble("km"));
+		this.setTariffaKm(resultSet.getDouble("tariffa_km"));
+		this.setNomeAutostrada(resultSet.getString("nome_autostrada"));
+		this.setInizio(resultSet.getString("inizio"));
+		this.setFine(resultSet.getString("fine"));
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Autostrada))
-			return false;
-		Autostrada other = (Autostrada) obj;
-		if (fine == null) {
-			if (other.fine != null)
-				return false;
-		} else if (!fine.equals(other.fine))
-			return false;
-		if (autostrade == null) {
-			if (other.autostrade != null)
-				return false;
-		} else if (!autostrade.equals(other.autostrade))
-			return false;
-		if (inizio == null) {
-			if (other.inizio != null)
-				return false;
-		} else if (!inizio.equals(other.inizio))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (km == null) {
-			if (other.km != null)
-				return false;
-		} else if (!km.equals(other.km))
-			return false;
-		if (nome_autostrada == null) {
-			if (other.nome_autostrada != null)
-				return false;
-		} else if (!nome_autostrada.equals(other.nome_autostrada))
-			return false;
-		if (tariffa_km == null) {
-			if (other.tariffa_km != null)
-				return false;
-		} else if (!tariffa_km.equals(other.tariffa_km))
-			return false;
-		return true;
-	}
+	//metodi di get e set
+	
+	public int getId() {return id;}
+	public void setId(int id) {this.id=id;}
+	
+	public double getKm() {return km;}
+	public void setKm(double km) {this.km=km;}
+	
+	public double getTariffaKm() {return tariffa_km;}
+	public void setTariffaKm(double tariffa_km) {this.tariffa_km=tariffa_km;}
+	
+	public String getNomeAutostrada() {return nome_autostrada;}
+	public void setNomeAutostrada(String nome_autostrada) {this.nome_autostrada=nome_autostrada;}
+	
+	public String getInizio() {return inizio;}
+	public void setInizio(String inizio) {this.inizio=inizio;}
+	
+	public String getFine() {return fine;}
+	public void setFine(String fine) {this.fine=fine;}
 	
 	
-	
-	
-	
+
+
 }
--- EcoToll/src/application/model/Casello.java
+++ EcoToll/src/application/model/Casello.java
@@ -1,87 +1,51 @@
 package application.model;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Casello {
 	
-	private IntegerProperty id = new SimpleIntegerProperty();
-	private StringProperty nome_casello = new SimpleStringProperty();
-	private IntegerProperty altezza_km = new SimpleIntegerProperty();
-	private StringProperty id_autostrada = new SimpleStringProperty();
-	private Autostrada autostrada;
+		
+		private int id,id_autostrada;
+		private double altezza_km;
+		private String nome_casello;
+		
+		//costruttore vuoto
+		
+		public Casello() {}
+		
+		//costruttore che contiene tutti i campi
+		public Casello(int id,int id_autostrada, double altezza_km,String nome_casello) {
+			this.id= id;
+			this.id_autostrada= id_autostrada;
+			this.altezza_km= altezza_km;
+			this.nome_casello= nome_casello;
+		}
+		
+		//costruttore che contiene ResultSet (query DB)
+		
+		public Casello(ResultSet resultSet) throws SQLException{
+			this.setId(resultSet.getInt("id"));
+			this.setIdAutostrada(resultSet.getInt("id_autostrada"));
+			this.setAltezzaKm(resultSet.getDouble("altezza_km"));
+			this.setNomeCasello(resultSet.getString("nome_casello"));
+		}
+		
+		//metodi di get e set
+		
+		public int getId() {return id;}
+		public void setId(int id) {this.id=id;}
+		
+		public int getIdAutostrada() {return id_autostrada;}
+		public void setIdAutostrada(int id_autostrada) {this.id_autostrada=id_autostrada;}
+		
+		public double getAltezzaKm() {return altezza_km;}
+		public void setAltezzaKm(double altezza_km) {this.altezza_km=altezza_km;}
+		
+		public String getNomeCasello() {return nome_casello;}
+		public void setNomeCasello(String nome_casello) {this.nome_casello=nome_casello;}
+		
 	
-	public Casello() {
-		id = new SimpleIntegerProperty();
-		nome_casello = new SimpleStringProperty();
-		altezza_km = new SimpleIntegerProperty();
-		id_autostrada = new SimpleStringProperty();
-		autostrada = new Autostrada();
-	}
-	
-	@Override
-	public String toString() {
-		return getNomeCasello();
-	}
-	
-	//metodi getter
-	public final int getIdCasello() { return id.get();}
-	public final String getNomeCasello() {return nome_casello.get();}
-	public final int getAltezzaKm() {return altezza_km.get();}
-	public final String getIdAutostrada() {return id_autostrada.get();}
-	
-	//metodi setter
-	
-	public final void setIdCasello(int value) {id.set(value);}
-	public final void setNomeCasello(String value) {nome_casello.set(value);}
-	public final void setAltezzaKm(int value) {altezza_km.set(value);}
-	public final void setIdAutostrada(String value) {id_autostrada.set(value);}
-	
-	//metodi getter per i "Property"
-	
-	public IntegerProperty idCaselloProperty() {return id;}
-	public StringProperty NomeCaselloProperty() {return nome_casello;}
-	public IntegerProperty AltezzaKmProperty() {return altezza_km;}
-	public StringProperty IdAutostradaProperty() {return id_autostrada;}
-	
-	public Autostrada getAutostrada() {return this.autostrada;}
-	
-	public void setAutostrada(Autostrada autostrada) {this.autostrada=autostrada;}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Casello))
-			return false;
-		Casello other = (Casello) obj;
-		if (altezza_km == null) {
-			if (other.altezza_km != null)
-				return false;
-		} else if (!altezza_km.equals(other.altezza_km))
-			return false;
-		if (autostrada == null) {
-			if (other.autostrada != null)
-				return false;
-		} else if (!autostrada.equals(other.autostrada))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nome_casello == null) {
-			if (other.nome_casello != null)
-				return false;
-		} else if (!nome_casello.equals(other.nome_casello))
-			return false;
-		return true;
 	}
 
-	
 
-}
